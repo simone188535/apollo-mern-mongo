fc8c63d9e9d768c3452465c000916cb379f795b7
Simone Anthony <simone.anthony1@yahoo.com>
2022-02-10 09:37:06 -0500

    unneeded code removed, db name changed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/users',
+  process.env.MONGODB_URI || 'mongodb://localhost/vinylCollection',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
